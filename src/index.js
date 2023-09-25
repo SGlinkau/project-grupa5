0abef551846b53dc1d70947347e18341c856a0e6
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-09-25 21:57:43 +0200

    Popular movies api request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './main.scss';
+import axios from 'axios';
+import { getPopular } from './api.js';
+// import { getByTitle } from './api.js';
+
+axios.defaults.headers.common['x-api-key'] = 'c90cdec037818042646f6ab3cec9ea66';
+
+const input = document.querySelector('#input');
+
+window.addEventListener('load', getPopular);
+input.addEventListener('submit', e => {
+  e.preventDefault();
+  getByTitle(input.value);
+});
