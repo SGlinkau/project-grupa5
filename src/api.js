4f254efe648d0a49c2ede7c723632d6f6675d48e
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-09-27 00:07:10 +0200

    Fetching movies details function added

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const header = document.querySelector('.header');
 export const moviesList = document.createElement('ul');
+moviesList.classList.add('movies-list');
 
 export async function getPopular() {
   try {
@@ -21,7 +22,13 @@ export async function getPopular() {
     popular.data.results.forEach(item => {
       moviesList.insertAdjacentHTML(
         'beforeend',
-        `<li><a>${item.title}</a></li>`
+        `<li class='movie-box'>
+        <a class='movie-box__link'>
+        <img class='movie-box__poster' id=${item.id} src='https://www.themoviedb.org/t/p/w500${item.poster_path}' />
+        <h2 class='movie-box__title'>${item.title}</h2>
+        <p class='movie-box__info'></p>
+        </a>
+        </li>`
       );
     });
   } catch {
@@ -46,9 +53,39 @@ export async function getByTitle(title) {
     moviesByTitle.data.results.forEach(item => {
       moviesList.insertAdjacentHTML(
         'beforeend',
-        `<li><a>${item.title}</a></li>`
+        `<li class='movie-box'>
+        <a class='movie-box__link'>
+        <img class='movie-box__poster' id=${item.id} src='https://www.themoviedb.org/t/p/w500${item.poster_path}' />
+        <h2 class='movie-box__title'>${item.title}</h2>
+        <p class='movie-box__info'></p>
+        </a>
+        </li>`
       );
     });
+  } catch {
+    error => console.log(error);
+  }
+}
+
+export async function getDetails(movieId) {
+  try {
+    const movieBox = document.createElement('div');
+    moviesList.after(movieBox);
+    const details = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}`,
+      {
+        params: {
+          language: 'en-US',
+          api_key: 'c90cdec037818042646f6ab3cec9ea66',
+        },
+        headers: { accept: 'application/json' },
+      }
+    );
+    movieBox.insertAdjacentHTML(
+      'afterbegin',
+      `<h2>${details.data.title}</h2>
+    <p>ABOUT: ${details.data.overview}</p>`
+    );
   } catch {
     error => console.log(error);
   }
