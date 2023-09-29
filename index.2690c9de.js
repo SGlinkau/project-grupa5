d5c733d32720a432d36d1eddcee3445e7aeacd21
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-09-29 08:11:04 +0000

    Deploying to gh-pages from @ SGlinkau/project-grupa5@4d67f573a22909e32929d70be096f239396276a4 🚀

---
+++ index.2690c9de.js
@@ -0,0 +1,3 @@
+let e=()=>{let e=document.getElementById("progress");document.getElementById("progress-value");let t=document.documentElement.scrollTop,n=Math.round(100*t/(document.documentElement.scrollHeight-document.documentElement.clientHeight));t>100?e.style.display="grid":e.style.display="none",e.addEventListener("click",()=>{document.documentElement.scrollTop=0}),e.style.background=`conic-gradient(#03ca65 ${n}%, #d7d7d7 ${n}%)`};window.onscroll=e,window.onload=e;//# sourceMappingURL=index.2690c9de.js.map
+
+//# sourceMappingURL=index.2690c9de.js.map
