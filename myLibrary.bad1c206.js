0f3f3facb0a3acf3f0879d3d2709da35b62a561e
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-10-04 15:01:10 +0000

    Deploying to gh-pages from @ SGlinkau/project-grupa5@053460f44f7156bfde8d56c790b65ab1178a905a 🚀

--- myLibrary.bad1c206.js
+++ myLibrary.bad1c206.js
@@ -1,4 +1,11 @@
-var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},t={},a={},i=e.parcelRequire9b51;null==i&&((i=function(e){if(e in t)return t[e].exports;if(e in a){var i=a[e];delete a[e];var r={id:e,exports:{}};return t[e]=r,i.call(r.exports,r,r.exports),r.exports}var o=Error("Cannot find module '"+e+"'");throw o.code="MODULE_NOT_FOUND",o}).register=function(e,t){a[e]=t},e.parcelRequire9b51=i);var r=i("2shzp"),o=i("2POL7");const n=document.querySelector(".library-list"),l=document.querySelector("#watched-btn");async function d(){let e=JSON.parse(localStorage.getItem("watchedList"));e.forEach(async e=>{let t=await (0,r.default).get(`https://api.themoviedb.org/3/movie/${e.id}`,{params:{language:"en-US",api_key:"c90cdec037818042646f6ab3cec9ea66"},headers:{accept:"application/json"}}),a=[];t.data.genres.forEach(e=>a.push(e.name));let i=t.data.poster_path?`https://www.themoviedb.org/t/p/w500${t.data.poster_path}`:noPosterURL;n.insertAdjacentHTML("beforeend",`<li class='movie-box'>
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},t={},a={},i=e.parcelRequire9b51;null==i&&((i=function(e){if(e in t)return t[e].exports;if(e in a){var i=a[e];delete a[e];var o={id:e,exports:{}};return t[e]=o,i.call(o.exports,o,o.exports),o.exports}var r=Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r}).register=function(e,t){a[e]=t},e.parcelRequire9b51=i);var o=i("2shzp"),r=i("2POL7");const n=document.querySelector(".library-list"),l=document.querySelector("#watched-btn"),s=document.querySelector("#queue-btn");async function c(){n.replaceChildren();let e=JSON.parse(localStorage.getItem("watchedList"));e.forEach(async e=>{let t=await (0,o.default).get(`https://api.themoviedb.org/3/movie/${e.id}`,{params:{language:"en-US",api_key:"c90cdec037818042646f6ab3cec9ea66"},headers:{accept:"application/json"}}),a=[];t.data.genres.forEach(e=>a.push(e.name));let i=t.data.poster_path?`https://www.themoviedb.org/t/p/w500${t.data.poster_path}`:noPosterURL;n.insertAdjacentHTML("beforeend",`<li class='movie-box'>
+      <a class='movie-box__link'>
+      <button class='movie-box__trailer-button' type='button' id=${e.id}>Trailer</button>
+    <img class='movie-box__poster' id=${e.id} src='${i}' />
+    </a>
+    <h2 class='movie-box__title'>${t.data.title}</h2>
+    <p class='movie-box__info'> ${[...a]} | ${t.data.release_date.slice(0,4)}</p>
+    </li>`)})}async function d(){n.replaceChildren();let e=JSON.parse(localStorage.getItem("queueList"));e.forEach(async e=>{let t=await (0,o.default).get(`https://api.themoviedb.org/3/movie/${e.id}`,{params:{language:"en-US",api_key:"c90cdec037818042646f6ab3cec9ea66"},headers:{accept:"application/json"}}),a=[];t.data.genres.forEach(e=>a.push(e.name));let i=t.data.poster_path?`https://www.themoviedb.org/t/p/w500${t.data.poster_path}`:noPosterURL;n.insertAdjacentHTML("beforeend",`<li class='movie-box'>
       <a class='movie-box__link'>
       <button class='movie-box__trailer-button' type='button' id=${e.id}>Trailer</button>
     <img class='movie-box__poster' id=${e.id} src='${i}' />
@@ -59,12 +66,6 @@ var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"u
 //     listContainer.appendChild(ul);
 //   }
 // }
-// watchedButton.addEventListener('click', () => {
-//   displayList('watched');
-// });
-// queueButton.addEventListener('click', () => {
-//   displayList('queue');
-// });
-//# sourceMappingURL=myLibrary.f31d3818.js.map
-window.addEventListener("load",()=>{d()}),l.addEventListener("click",()=>{d()}),n.addEventListener("click",e=>{if("IMG"!==e.target.nodeName)return;let t=e.target.getAttribute("id");(0,o.getDetails)(t)}),n.addEventListener("click",e=>{if("BUTTON"!==e.target.nodeName)return;let t=e.target.getAttribute("id");(0,o.getTrailer)(t)});
-//# sourceMappingURL=myLibrary.f31d3818.js.map
+//# sourceMappingURL=myLibrary.bad1c206.js.map
+s.addEventListener("click",()=>{d()}),l.addEventListener("click",()=>{c()}),n.addEventListener("click",e=>{if("IMG"!==e.target.nodeName)return;let t=e.target.getAttribute("id");(0,r.getDetails)(t)}),n.addEventListener("click",e=>{if("BUTTON"!==e.target.nodeName)return;let t=e.target.getAttribute("id");(0,r.getTrailer)(t)});
+//# sourceMappingURL=myLibrary.bad1c206.js.map
